76b3e2a1095b52ba75af2fd897be8cdc809721ea
Marian <toni_kukoc@gmx.de>
2021-11-26 17:29:58 +0100

    :sparkles: Created DiscordbBot

--- src/main/java/com/github/crafttogether/chatbridge/ChatBridge.java
+++ src/main/java/com/github/crafttogether/chatbridge/ChatBridge.java
@@ -1,13 +1,24 @@
 package com.github.crafttogether.chatbridge;
 
+import com.github.crafttogether.chatbridge.discord.DiscordBot;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.security.auth.login.LoginException;
 
 public final class ChatBridge extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        try {
+            plugin = this;
+            DiscordBot.start();
+            Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "ChatBridge is active");
+        } catch (LoginException e) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Error while starting the Plugin \"ChatBridge\"");
+            e.printStackTrace();
+        }
     }
 
     @Override
---
+++ src/main/java/com/github/crafttogether/chatbridge/discord/DiscordBot.java
@@ -0,0 +1,18 @@
+package com.github.crafttogether.chatbridge.discord;
+
+import com.github.crafttogether.chatbridge.ChatBridge;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+public class DiscordBot {
+
+    public static void start() throws LoginException {
+        JDABuilder
+                .createLight(ChatBridge.plugin.getConfig().getString("token"))
+                .setEnabledIntents(GatewayIntent.GUILD_MESSAGES)
+                .build();
+    }
+
+}
