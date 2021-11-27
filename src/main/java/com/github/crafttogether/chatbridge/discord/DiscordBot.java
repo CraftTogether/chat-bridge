9dd9eb366342291f67aad5ef05438f9cdbedf75d
Marian <toni_kukoc@gmx.de>
2021-11-27 16:24:43 +0100

    :wrench: Changed configuration file structure

--- src/main/java/com/github/crafttogether/chatbridge/discord/DiscordBot.java
+++ src/main/java/com/github/crafttogether/chatbridge/discord/DiscordBot.java
@@ -4,15 +4,18 @@ import com.github.crafttogether.chatbridge.ChatBridge;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import org.bukkit.Bukkit;
 
 public class DiscordBot {
 
     public static void start() {
-        final DiscordClient client = DiscordClient.create(ChatBridge.plugin.getConfig().getString("token"));
+        Bukkit.getConsoleSender().sendMessage("Connecting to Discord");
+
+        final String token = ChatBridge.plugin.getConfig().getConfigurationSection("discord").getString("token");
+        final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
         final MessageListener listener = new MessageListener();
-
         gateway.on(MessageCreateEvent.class).subscribe(listener::onGuildMessageReceived);
     }
 
