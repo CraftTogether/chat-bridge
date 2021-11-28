c430118b737e48b52bc0ee07ec591619e4ae7033
Marian <toni_kukoc@gmx.de>
2021-11-28 15:10:08 +0100

    :sparkles: Made Discord > Minecraft working

--- src/main/java/com/github/crafttogether/chatbridge/discord/MessageListener.java
+++ src/main/java/com/github/crafttogether/chatbridge/discord/MessageListener.java
@@ -9,7 +9,8 @@ public class MessageListener {
 
     public void onGuildMessageReceived(@NotNull MessageCreateEvent event) {
         if (event.getMessage().getWebhookId().isPresent()) return;
-        if (!ChatBridge.plugin.getConfig().getString("discordChannelId").equals(event.getMessage().getRestChannel().getId().asString())) return;
+        final String channelId = ChatBridge.plugin.getConfig().getConfigurationSection("discord").getString("discordChannelId");
+        if (!channelId.equals(event.getMessage().getRestChannel().getId().asString())) return;
 
         MinecraftMessageHandler.handle(event.getMember().get().getDisplayName(), event.getMessage().getContent());
         //IrcMessageHandler.handle()
--- src/main/java/com/github/crafttogether/chatbridge/minecraft/MinecraftMessageHandler.java
+++ src/main/java/com/github/crafttogether/chatbridge/minecraft/MinecraftMessageHandler.java
@@ -1,9 +1,14 @@
 package com.github.crafttogether.chatbridge.minecraft;
 
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.Bukkit;
+
 public class MinecraftMessageHandler {
 
-    public static void handle(String name, String message) {
-
+    public static void handle(String author, String message) {
+        TextComponent msg = PlainTextComponentSerializer.plainText().deserialize(String.format("<%s> %s", author, message));
+        Bukkit.getServer().sendMessage(msg);
     }
 
 }
