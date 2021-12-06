7732ac453fac86e1954260f1f4e6337ef51286ac
Polaris <polariandev@protonmail.com>
2021-12-06 19:58:36 +0000

    bumped ircj snapshot version

--- libs/install-dependency.bat
+++ libs/install-dependency.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=./ircj-1.0-SNAPSHOT.jar -DgroupId=dev.polarian -DartifactId=ircj -Dversion=1.0-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile="ircj-1.0-SNAPSHOT-4.jar" -DgroupId="dev.polarian" -DartifactId="ircj" -Dversion="1.0-SNAPSHOT-4" -Dpackaging="jar"
