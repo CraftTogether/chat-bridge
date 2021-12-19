637d092930a988a5dc91f0f42015a5b76fb2c3ba
Marian <elias-marian@web.de>
2021-12-19 17:44:51 +0100

    :zap: Made sending discord messages in a new thread

--- src/main/java/com/github/crafttogether/chatbridge/discord/DiscordMessageSender.java
+++ src/main/java/com/github/crafttogether/chatbridge/discord/DiscordMessageSender.java
@@ -4,23 +4,24 @@ import com.github.crafttogether.chatbridge.ChatBridge;
 import com.github.crafttogether.chatbridge.utilities.Webhook;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.util.ChatPaginator;
 
 import java.io.IOException;
 
 public class DiscordMessageSender {
 
     public static void send(String username, String message, String avatar, MessageSource source) {
-        try {
-            new Webhook(ChatBridge.plugin.getConfig().getConfigurationSection("discord").getString("webhook"))
-                    .setAvatarUrl(avatar)
-                    .setUsername(username)
-                    .setContent(source.icon + " " + message)
-                    .send();
-        } catch (IOException e) {
-            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Error while sending a message to Discord");
-            e.printStackTrace();
-        }
+        new Thread(() -> {
+            try {
+                new Webhook(ChatBridge.plugin.getConfig().getConfigurationSection("discord").getString("webhook"))
+                        .setAvatarUrl(avatar)
+                        .setUsername(username)
+                        .setContent(source.icon + " " + message)
+                        .send();
+            } catch (IOException e) {
+                Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Error while sending a message to Discord");
+                e.printStackTrace();
+            }
+        }).start();
     }
 
 }
