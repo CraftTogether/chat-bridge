c84427262bd1225af218c211958c722ca134c830
Marian <elias-marian@web.de>
2021-12-28 13:12:53 +0100

    :bug: Added threading on discord messages again (otherwise it would throw weird errors)

--- src/main/java/com/github/crafttogether/chatbridge/discord/DiscordMessageSender.java
+++ src/main/java/com/github/crafttogether/chatbridge/discord/DiscordMessageSender.java
@@ -10,16 +10,19 @@ import java.io.IOException;
 public class DiscordMessageSender {
 
     public static void send(String username, String message, String avatar, MessageSource source) {
-        try {
-            new Webhook(ChatBridge.plugin.getConfig().getConfigurationSection("discord").getString("webhook"))
-                    .setAvatarUrl(avatar)
-                    .setUsername(username)
-                    .setContent(source.icon + " " + message)
-                    .send();
-        } catch (IOException e) {
-            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Error while sending a message to Discord");
-            e.printStackTrace();
-        }
+        new Thread(() -> {
+            try {
+                System.out.println(avatar);
+                new Webhook(ChatBridge.plugin.getConfig().getConfigurationSection("discord").getString("webhook"))
+                        .setAvatarUrl(avatar)
+                        .setUsername(username)
+                        .setContent(source.icon + " " + message)
+                        .send();
+            } catch (IOException e) {
+                Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Error while sending a message to Discord");
+                e.printStackTrace();
+            }
+        }).start();
     }
 
 }
