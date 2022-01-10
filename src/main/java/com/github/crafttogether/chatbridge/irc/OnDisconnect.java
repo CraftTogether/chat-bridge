882d9a4fc0e780d2d5e82c2f04c778974bca73f7
Polaris <polariandev@protonmail.com>
2022-01-10 12:41:21 +0000

    added disconnect message to chat

--- src/main/java/com/github/crafttogether/chatbridge/irc/OnDisconnect.java
+++ src/main/java/com/github/crafttogether/chatbridge/irc/OnDisconnect.java
@@ -2,10 +2,14 @@ package com.github.crafttogether.chatbridge.irc;
 
 import com.github.crafttogether.chatbridge.ChatBridge;
 import dev.polarian.ircj.events.DisconnectEvent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 public class OnDisconnect implements DisconnectEvent {
     @Override
     public void invoke() {
         ChatBridge.setIrcConnected(false);
+        Bukkit.getServer().sendMessage(PlainTextComponentSerializer.plainText().deserialize(ChatColor.RED + "[Chat Bridge]: Disconnected from IRC server!"));
     }
 }
