428bd6457e5812b1a3aada1d697ce85d9bd09424
PolarianDev <polariandev@protonmail.com>
2022-02-24 09:19:52 +0000

    fixed invalid statistics in IRC channel topic

--- src/main/java/xyz/crafttogether/chatbridge/minecraft/listeners/MinecraftQuitEvent.java
+++ src/main/java/xyz/crafttogether/chatbridge/minecraft/listeners/MinecraftQuitEvent.java
@@ -3,6 +3,7 @@ package xyz.crafttogether.chatbridge.minecraft.listeners;
 import dev.polarian.ircj.utils.Formatting;
 import net.dv8tion.jda.api.EmbedBuilder;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -22,7 +23,7 @@ public class MinecraftQuitEvent implements Listener {
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        ChatBridge.updateIrcChannelStatistics(Bukkit.getOnlinePlayers().size(), Weg.getAfkPlayers());
+        ChatBridge.updateIrcChannelStatistics(Bukkit.getOnlinePlayers().size() - 1, Weg.getAfkPlayers());
         final EmbedBuilder embed = new EmbedBuilder()
                 .setColor(Color.RED)
                 .setTitle(String.format("%s has left the server", event.getPlayer().getName()));
